aef304f351253d1374ebed74f2694b3d549bbc1d
Alejandra Cime <221k0191@itscarrillopuerto.edu.mx>
2026-05-03 20:27:06 -0500

    Actividad 3.2 modificado realizada

--- TemaTres_ BBDD_ORM/3.2 API CRUD/index.js
+++ TemaTres_ BBDD_ORM/3.2 API CRUD/index.js
@@ -4,8 +4,6 @@ import Usuario from './models/usuario.model.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-
 const app = express();
 const puerto = 3000;
 
@@ -15,32 +13,26 @@ app.use(express.static('public'));
 
 app.post('/usuarios', async (req, res) => {
     try{
-    const usuario = await Usuario.create(req.body);
-    res.status(201).json(usuario);
+        const usuario = await Usuario.create(req.body);
+        res.status(201).json(usuario);
     } catch (error) {
-    console.error("Error al crear el usuario:", error);
-    res.status(500).json({ error: 'Error al crear el usuario' });
+        console.error("Error al crear el usuario:", error);
+        res.status(500).json({ error: 'Error al crear el usuario' });
     }
 });
 
-// Ruta básica
 app.get('/', (req, res) => {
   res.send('Bienvenido a mi API CRUD');
 });
 
-// Iniciar servidor
-app.listen(puerto, () => {
-  console.log(`Servidor escuchando en http://localhost:${puerto}`);
-});
-
-
-// Define la URI (corregida)
 const uri = process.env.uri;
 
-// Conecta a la base de datos
 mongoose.connect(uri)
   .then(() => {
     console.log("Conexión exitosa a la base de datos");
+    app.listen(puerto, () => {
+      console.log(`Servidor escuchando en http://localhost:${puerto}`);
+    });
   })
   .catch((error) => {
     console.error("Error al conectar a la base de datos:", error);
